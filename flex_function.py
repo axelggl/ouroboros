43911726df6029ca0f215a5abd1c36676a259f55
maxel <axel.maerten@ynov.com>
2023-10-10 21:14:20 +0200

    personne

---
+++ flex_function.py
@@ -0,0 +1,10 @@
+def create_person(first_name, last_name, age=None, gender=None, size=1.83, job="taxidermist"):
+    person = {
+    'first_name': 'Kevin',
+    'last_name': 'Boulin',
+    'age': 34,
+    'gender': 'male',
+    'size': 1.83,
+    'job': 'taxidermist',
+    }
+    return person
