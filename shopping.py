b17dc09ce21f8205083204fab029953e273ad7b2
maxel <axel.maerten@ynov.com>
2023-10-10 16:42:34 +0200

    la liste mais en plus joli en fait

--- shopping.py
+++ shopping.py
@@ -11,10 +11,11 @@ def clean_list(shopping_list):
 
     if not shopping_list:
         return []
+    
+    if 'milk' not in shopping_list:
+        shopping_list.append('milk')
 
     for index, item in enumerate(shopping_list, start=1):
-        if 'milk' not in shopping_list:
-            shopping_list.append('milk')
         cleaned_item = item.strip().rstrip().capitalize()
         formatted_item = f"{index}/ {cleaned_item}"
         final_list.append(formatted_item)
