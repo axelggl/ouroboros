7a3f4bbbb274df4a218b6250527303326ba6b3a6
maxel <axel.maerten@ynov.com>
2023-10-10 15:47:19 +0200

    la liste mais en plus joli en fait

--- shopping.py
+++ shopping.py
@@ -5,3 +5,19 @@ def remember_the_milk(shopping_list):
     if 'milk' not in shopping_list:
         shopping_list.append('milk')
     return shopping_list
+
+def clean_list(shopping_list):
+    final_list = []
+
+    if not shopping_list:
+        return []
+    
+    if 'milk' not in shopping_list:
+        shopping_list.append('milk')
+
+    for index, item in enumerate(shopping_list, start=1):
+        cleaned_item = item.strip().capitalize()
+        formatted_item = f"{index}/ {cleaned_item}"
+        final_list.append(formatted_item)
+
+    return final_list
