d4a37c0f9a559dae6a69c8041d1702027df11263
maxel <axel.maerten@ynov.com>
2023-10-10 14:46:42 +0200

    mathématiques

---
+++ numerical_operations.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    res = a + b
+    return res
+
+def subtract(a, b):
+    res = a - b
+    return res
+
+def multiply(a, b):
+    res = a * b
+    return res
+
+def power(a, b):
+    res = a ** b
+    return res
+
+def square(a):
+    res = a ** 2
+    return res
