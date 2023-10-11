f88b141b8328088202b6cbc044be0b77a28b2f89
maxel <axel.maerten@ynov.com>
2023-10-11 14:28:26 +0200

    les ingrédients

--- recipes.py
+++ recipes.py
@@ -1,7 +1,7 @@
 def create_recipe(name, persons, ingredients):
     if len(name) > 150: 
         raise ValueError("Title is too long")
-    if persons is None or persons > 50:
+    if persons is None or persons == 0 or persons > 50:
         raise ValueError("Invalid persons number")
     if not ingredients:
         raise ValueError("This recipe has no ingredients")
