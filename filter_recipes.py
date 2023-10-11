fafc1e9b99e00619c23a4a61e7474f88ddb56b2f
maxel <axel.maerten@ynov.com>
2023-10-11 15:46:19 +0200

    alors

---
+++ filter_recipes.py
@@ -0,0 +1,1 @@
+def filter_recipes(recipes, max_persons): return [recipe['title'] for recipe in recipes if recipe['persons'] < max_persons]
--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import datetime as dt
 def build_menu(recipes: list[dict], start_date: dt.date) -> list[tuple[dt.date, str]]:
     menu = []
     current_date = start_date
-
+    
     for recipe in recipes:
         title = recipe['title']
         menu.append((current_date, title))
