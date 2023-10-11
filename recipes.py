453bea8123b66164b60fd0b609d524be8770e5c9
maxel <axel.maerten@ynov.com>
2023-10-11 16:18:28 +0200

    fortinaite i la babajé

--- recipes.py
+++ recipes.py
@@ -13,3 +13,18 @@ def create_recipe(name, persons, ingredients):
         }
 
     return recipe_data
+
+def create_recipe_v2(title, persons, *ingredients, **tags):
+    if len(title) > 40:
+        raise ValueError("Title is too long")
+    if persons > 10:
+        raise ValueError("Too many people")
+    if not ingredients:
+        raise ValueError("There are no ingredients")
+    
+    return {
+        'title': title,
+        'persons': persons,
+        'ingredients': ingredients,
+        'tags': tags
+    }
