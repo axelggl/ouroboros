1bb9ed257003c4bbe8c3e5c253d9e6f9ffbecbc7
maxel <axel.maerten@ynov.com>
2023-10-11 17:28:36 +0200

    oh j'en peux plus

--- decorator.py
+++ decorator.py
@@ -3,6 +3,6 @@ import datetime as dt
 def with_current_date(function):
     def modified_function(*args, **kwargs):
         current_date = dt.date.today()
-        return function(args, current_date=current_date, **kwargs)
+        return function(*args, current_date=current_date, **kwargs)
 
     return modified_function
