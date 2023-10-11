ca819b4221b43f3777906c2bccaf5d0933ed5917
maxel <axel.maerten@ynov.com>
2023-10-11 15:36:32 +0200

    je sais pas

---
+++ menu.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+def build_menu(recipes: list[dict], start_date: dt.date) -> list[tuple[dt.date, str]]:
+    menu = []
+    current_date = start_date
+
+    for recipe in recipes:
+        title = recipe['title']
+        menu.append((current_date, title))
+        current_date += dt.timedelta(days=1)
+
+    return menu
