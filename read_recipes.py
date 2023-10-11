446fc7500fa59c0c049c39e79f0f68b606c91034
maxel <axel.maerten@ynov.com>
2023-10-11 14:41:44 +0200

    jason

---
+++ read_recipes.py
@@ -0,0 +1,12 @@
+import json
+
+def get_recipes(file_name):
+    try:
+        with open(file_name, encoding='utf-8') as file:
+            recipes_data = json.load(file)
+        return recipes_data
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON in '{file_name}'.")
+    return []
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-import flex_function
+import read_recipes
 
 if __name__ == '__main__':
-    print(flex_function.create_person('Kevin', 'Boulin', 34, 'male', size=1.83, job='taxidermist'))
-    print(flex_function.create_person('Kevin', 'Boulin', age=34, gender='male', size=1.83, job='taxidermist'))
-    print(flex_function.create_person('Kevin', 'Boulin', 34, 'male'))
+    print(read_recipes.get_recipes('recipes_data.json'))
