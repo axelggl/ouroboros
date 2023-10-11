0686b5a3fefb77ed4c5940de9ada75ccbe625380
maxel <axel.maerten@ynov.com>
2023-10-11 15:50:40 +0200

    on écrit sur les murs le nom de ceux qu'on aime

--- menu.py
+++ menu.py
@@ -3,10 +3,16 @@ import datetime as dt
 def build_menu(recipes: list[dict], start_date: dt.date) -> list[tuple[dt.date, str]]:
     menu = []
     current_date = start_date
-    
+
     for recipe in recipes:
         title = recipe['title']
         menu.append((current_date, title))
         current_date += dt.timedelta(days=1)
 
     return menu
+
+def save_menu(meals: list[tuple[dt.date, str]]):
+    with open("menu.txt", "w") as file:
+        for date, recipe in meals:
+            formatted_date = date.strftime("%A %d %B %Y")
+            file.write(f"{formatted_date}: {recipe}\n")
