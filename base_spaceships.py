8b273b25a3d52b373e14eb23eb79ef5cd5053c34
maxel <axel.maerten@ynov.com>
2023-10-12 16:00:46 +0200

    je construis les constructeurs

--- base_spaceships.py
+++ base_spaceships.py
@@ -1,5 +1,7 @@
 class Spaceship:
-    def __init__(self, is_alive=True, attack=0, defense=0):
-        self.is_alive = is_alive
+    def __init__(self, attack=0, defense=0):
+        self.is_alive = True
         self.attack = attack
         self.defense = defense
+
+    
