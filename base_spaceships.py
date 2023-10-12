a3807fd30c1d4d688226929e3a2660ce4f9dc351
maxel <axel.maerten@ynov.com>
2023-10-12 15:56:33 +0200

    les choses

---
+++ base_spaceships.py
@@ -0,0 +1,5 @@
+class Spaceship:
+    def __init__(self, is_alive=True, attack=0, defense=0):
+        self.is_alive = is_alive
+        self.attack = attack
+        self.defense = defense
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import datetime as dt
-from decorator import with_current_date
-
-@with_current_date
-def log_message(message: str, current_date: dt.date):
-    # this is an example function
-    # you can imagine any other functions
-    return f'{current_date}: {message}'
-
 if __name__ == '__main__':
-    print(log_message('hello'))
-    print(log_message('world'))
+    print(Spaceship().is_alive)
+    print(Spaceship().attack)
+    print(Spaceship().defense)
