aaa0e55af0aaffda50cc735d1f33faa6d6606b35
maxel <axel.maerten@ynov.com>
2023-10-12 16:09:14 +0200

    les dégats chockbar

--- base_spaceships.py
+++ base_spaceships.py
@@ -1,13 +1,14 @@
 class Spaceship:
     is_alive = True
 
-    def __init__(self, attack=0, defense=0):
+    def init(self, attack=0, defense=0):
         self.attack = attack
         self.defense = defense
 
-    def take_damage(self, damage):
+    def take_damages(self, damage):
         if damage < 0:
             raise ValueError("Damage cannot be negative.")
+
         self.defense -= damage
         if self.defense <= 0:
             self.defense = 0
