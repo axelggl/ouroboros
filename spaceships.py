19d988a8b9a453f93f60064d283d0c2081a7dde4
axelggl <maerten.axel@ynov.com>
2023-10-13 12:12:15 +0200

    alors on danse

--- spaceships.py
+++ spaceships.py
@@ -22,43 +22,19 @@ class Interceptor(Fighter, FighterKiller):
     attack = 180
     defense = 1000
 
-    def __init__(self):
-        FighterKiller.__init__(self)
-        self.attack = Interceptor.attack
-        self.defense = Interceptor.defense
-
 class Bomber(Fighter, BattleshipKiller):
     attack = 150
     defense = 2000
-
-    def __init__(self):
-        BattleshipKiller.__init__(self)
-        self.attack = Bomber.attack
-        self.defense = Bomber.defense
 
 class Cruiser(Battleship, BattleshipKiller):
     attack = 800
     defense = 3000
 
-    def __init__(self):
-        BattleshipKiller.__init__(self)
-        self.attack = Cruiser.attack
-        self.defense = Cruiser.defense
-
 class Frigate(Battleship, FighterKiller):
     attack = 500
     defense = 2500
-
-    def __init__(self):
-        FighterKiller.__init__(self)
-        self.attack = Frigate.attack
-        self.defense = Frigate.defense
 
 class Destroyer(Battleship, BattleshipKiller):
     attack = 650
     defense = 5000
 
-    def __init__(self):
-        BattleshipKiller.__init__(self)
-        self.attack = Destroyer.attack
-        self.defense = Destroyer.defense
