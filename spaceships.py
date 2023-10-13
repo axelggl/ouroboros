d4eff7c6fdc928b29c3d2a93d67996049940b6a2
axelggl <maerten.axel@ynov.com>
2023-10-13 11:56:43 +0200

    j'ean

--- spaceships.py
+++ spaceships.py
@@ -4,47 +4,53 @@ class BattleshipKiller:
     def fire_on(self, target_spaceship):
         if isinstance(target_spaceship, Battleship):
             target_spaceship.take_damages(self.attack * 2)
-        else:
-            super().fire_on(target_spaceship)
 
 class FighterKiller:
     def fire_on(self, target_spaceship):
         if isinstance(target_spaceship, Fighter):
             target_spaceship.take_damages(self.attack * 2)
-        else:
-            super().fire_on(target_spaceship)
 
 class Interceptor(Fighter, FighterKiller):
     attack = 180
     defense = 1000
 
     def __init__(self):
-        super().__init__(self.attack, self.defense)
+        FighterKiller.__init__(self)
+        self.attack = Interceptor.attack
+        self.defense = Interceptor.defense
 
 class Bomber(Fighter, BattleshipKiller):
     attack = 150
     defense = 2000
 
     def __init__(self):
-        super().__init__(self.attack, self.defense)
+        BattleshipKiller.__init__(self)
+        self.attack = Bomber.attack
+        self.defense = Bomber.defense
 
 class Cruiser(Battleship, BattleshipKiller):
     attack = 800
     defense = 3000
 
     def __init__(self):
-        super().__init__(self.attack, self.defense)
+        BattleshipKiller.__init__(self)
+        self.attack = Cruiser.attack
+        self.defense = Cruiser.defense
 
 class Frigate(Battleship, FighterKiller):
     attack = 500
     defense = 2500
 
     def __init__(self):
-        super().__init__(self.attack, self.defense)
+        FighterKiller.__init__(self)
+        self.attack = Frigate.attack
+        self.defense = Frigate.defense
 
 class Destroyer(Battleship, BattleshipKiller):
     attack = 650
     defense = 5000
 
     def __init__(self):
-        super().__init__(self.attack, self.defense)
+        BattleshipKiller.__init__(self)
+        self.attack = Destroyer.attack
+        self.defense = Destroyer.defense
