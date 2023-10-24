9e2118fe64ddbb5e31d3d082a650b52b8f4aa266
maxel <axel.maerten@ynov.com>
2023-10-24 16:33:49 +0200

    etcaetera etcaetera

--- battle_simulator.py
+++ battle_simulator.py
@@ -4,14 +4,17 @@ import random
 class Simulator:
   @staticmethod
   def _duel_fight(attacker_ship: Spaceship, defender_ship: Spaceship):
-    attacker_ship.fire_on(defender_ship);
+    attacker_ship.fire_on(defender_ship)
 
     if defender_ship.is_alive == True:
       defender_ship.fire_on(attacker_ship)
 
   def _simulate_fight(self, attacker_ships, defender_ships):
-        for attacker in attacker_ships:
-            alive_defenders = [defender for defender in defender_ships if defender.is_alive]
-            if alive_defenders:
-                defender_ship = random.choice(alive_defenders)
-                self._duel_fight(attacker, defender_ship)
+        if not len(attacker_ships) or not len(defender_ships):
+            pass
+        else:
+            for attacker in attacker_ships:
+                alive_defenders = [defender for defender in defender_ships if defender.is_alive]
+                if alive_defenders:
+                    defender_ship = random.choice(alive_defenders)
+                    self._duel_fight(attacker, defender_ship)
